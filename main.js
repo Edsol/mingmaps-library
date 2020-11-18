06eca9f41f0c66c87be96d6aa577233bf03ba081
Edsol <edoardo.soloperto@gmail.com>
2020-11-18 13:21:23 +0100

    remove export lodash function

--- main.js
+++ main.js
@@ -5,7 +5,5 @@ import * as Route from './modules/route.js';
 import * as Infowindow from './modules/infowindow.js';
 import * as Utils from './modules/utils.js';
 
-import _ from "./node_modules/lodash-es/lodash.js"
-
-export { Map, Marker, Address, Route, Infowindow, Utils, _ };
+export { Map, Marker, Address, Route, Infowindow, Utils, };
 
