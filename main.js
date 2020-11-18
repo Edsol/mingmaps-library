4f3a207a667922a39dff7bb6fd76a24e7081d50c
Edsol <edoardo.soloperto@gmail.com>
2020-11-18 12:01:09 +0100

    add lodash module

--- main.js
+++ main.js
@@ -5,5 +5,7 @@ import * as Route from './modules/route.js';
 import * as Infowindow from './modules/infowindow.js';
 import * as Utils from './modules/utils.js';
 
-export { Map, Marker, Address, Route, Infowindow, Utils };
+import _ from "./node_modules/lodash-es/lodash.js"
+
+export { Map, Marker, Address, Route, Infowindow, Utils, _ };
 
